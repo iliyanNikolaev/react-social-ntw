1e2e15f05f8d09e9b09040d2ad7cd6427b6ebd66
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-21 13:44:25 +0200

    CreatePost component

---
+++ client/src/components/CreatePost/index.tsx
@@ -0,0 +1,25 @@
+import styles from './create-post.module.css';
+import { MdAddPhotoAlternate } from "react-icons/md";
+
+export const CreatePost = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}>
+        Add new post
+      </p>
+
+      <textarea className={styles.text} rows={2} cols={100}></textarea>
+
+      <label htmlFor='file' className={styles.addFile}>
+        <div className={styles.addFileBtn}>
+          add photo
+          <MdAddPhotoAlternate className={styles.addVisible} />
+        </div>
+
+        <input id='file' className={styles.addHidden} type="file" />
+      </label>
+
+      <button className={styles.btn}>post</button>
+    </div>
+  )
+}
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,57 +1,24 @@
+import { CreatePost } from '../../components/CreatePost';
 import styles from './home.module.css';
 
 export const Home = () => {
   return (
     <div className={styles.wrapper}>
         <div className={styles.container}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut facere, cum voluptate eius aliquam. Atque, dolor incidunt. Cupiditate corporis expedita incidunt suscipit omnis obcaecati numquam aperiam, magnam ad voluptatum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illum laudantium. Incidunt itaque et molestias nemo molestiae officia! Ea a officiis, veritatis minima aperiam quam aliquam at! Quas, totam iste.
+              <CreatePost />
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
+             <div className={styles.temp}>post</div>
         </div>
     </div>
   )
