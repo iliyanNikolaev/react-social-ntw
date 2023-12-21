c5e8d7d6e9f2934077adec28f9d7a497bad661d0
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-21 23:37:45 +0200

    css - media queries changes

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -4,6 +4,7 @@ import styles from './navbar.module.css';
 import { FaHome } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { IoShareSocial } from "react-icons/io5";
+import { FaUsers } from "react-icons/fa";
 
 import { Link } from 'react-router-dom';
 
@@ -19,6 +20,7 @@ export const Navbar = () => {
                 </div>
                 <div className={styles.icons}>
                     <Link to='/'><FaHome className={styles.homeIcon} /></Link>
+                    <Link to='/users'><FaUsers className={styles.usersIcon} /></Link>
                     <Link to='/profile/1'><CgProfile className={styles.profileIcon} /></Link>
                 </div>
             </div>
