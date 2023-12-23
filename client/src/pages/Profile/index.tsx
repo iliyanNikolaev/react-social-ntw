b024094a545595666b1ff0946b42521d4f4d29ee
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-23 14:48:20 +0200

    css - media query changes

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,7 +1,6 @@
-import { TbPlayerTrackNextFilled, TbPlayerTrackPrevFilled } from 'react-icons/tb';
 import styles from './profile.module.css';
 
-//icons
+import { TbPlayerTrackNextFilled, TbPlayerTrackPrevFilled } from 'react-icons/tb';
 import { SlUserFollow } from "react-icons/sl";
 
 import { useEffect, useState } from 'react';
@@ -9,63 +8,62 @@ import { useParams } from 'react-router-dom';
 import { IPost, IUser } from '../../interfaces';
 import { getUser, getPosts } from '../../data/service';
 import { PostList } from '../../components/PostList';
-import { toggleModal } from '../../components/Modal';
-import { ConnectionsModal } from '../../components/Modals/ConnectionsModal';
+import { ConnectionsModal, toggleConnectionsModal } from '../../components/Modals/ConnectionsModal';
 import { ScrollToTop } from '../../components/ScrollToTop';
 
 
 export const Profile = () => {
-  const { id } = useParams();
-  const [currentUser, setCurrentUser] = useState<IUser | undefined>();
-  const [currentPosts, setCurrentPosts] = useState<IPost[] | undefined>();
-  useEffect(() => {
-    const user = getUser(id);
-    setCurrentUser(user);
-    const posts = getPosts();
-    setCurrentPosts(posts);
-  }, [id]);
+    const { id } = useParams();
+    const [currentUser, setCurrentUser] = useState<IUser | undefined>();
+    const [currentPosts, setCurrentPosts] = useState<IPost[] | undefined>();
+    useEffect(() => {
+        const user = getUser(id);
+        setCurrentUser(user);
+        const posts = getPosts();
+        setCurrentPosts(posts);
+    }, [id]);
 
-  return (
-    <div className={styles.wrapper}>
-      <ScrollToTop />
-      <div className={styles.container}>
+    return (
+        <div className={styles.wrapper}>
+            <ScrollToTop />
+            <div className={styles.container}>
 
-        {/* modals */}
-        <ConnectionsModal connections={currentUser?.connections} />
+                {/* modals */}
+                <ConnectionsModal connections={currentUser?.connections} />
 
-        {!currentUser && <p>Loading...</p>}
+                {!currentUser && <p>Loading...</p>}
 
-        {currentUser && <>
-          <div className={styles.upper}>
-            <img className={styles.cover} src={currentUser.coverPic} alt="cover" />
-            <img className={styles.avatar} src={currentUser.profilePic} alt="avatar" />
+                {currentUser && <>
+                    <div className={styles.upper}>
+                        <img className={styles.cover} src={currentUser.coverPic} alt="cover" />
+                        <img className={styles.avatar} src={currentUser.profilePic} alt="avatar" />
 
-            <span className={styles.fullName}>{currentUser.firstName} {currentUser.lastName}</span>
-          </div>
+                        <span className={styles.fullName}>{currentUser.firstName} {currentUser.lastName}</span>
+                    </div>
 
-          <div className={styles.lower}>
-            <div className={styles.controls}>
-              <span
-                onClick={() => toggleModal('connections-modal')}
-                className={styles.connectionsBtn}
-              >
-                {currentUser.connections.length} connections
-              </span>
-              <button className={styles.btn}><SlUserFollow /> connect</button>
+                    <div className={styles.lower}>
+                        <div className={styles.controls}>
+                            <span
+                                onClick={toggleConnectionsModal}
+                                className={styles.connectionsBtn}
+                            >
+                                {currentUser.connections.length} connections
+                            </span>
+                            <button className={styles.btn}><SlUserFollow /> connect</button>
+                        </div>
+                    </div>
+
+                    <div className={styles.posts}>
+                        <PostList posts={currentPosts} />
+                    </div>
+
+                    <div className={styles.controlsPagination}>
+                        <button className={styles.btnPagination}><TbPlayerTrackPrevFilled /> {'Prev'}</button>
+                        <button className={styles.btnPagination}>{'Next'}<TbPlayerTrackNextFilled /></button>
+                    </div>
+                </>}
             </div>
-          </div>
 
-          <div className={styles.posts}>
-            <PostList posts={currentPosts} />
-          </div>
-
-          <div className={styles.controlsPagination}>
-            <button className={styles.btnPagination}><TbPlayerTrackPrevFilled /> {'Prev'}</button>
-            <button className={styles.btnPagination}>{'Next'}<TbPlayerTrackNextFilled /></button>
-          </div>
-        </>}
-      </div>
-
-    </div>
-  )
+        </div>
+    )
 }
